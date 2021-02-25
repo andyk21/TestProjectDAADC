ef4cf23f85eadf712b1313bd6aeffc4f770d9377
andyk21 <andykim2021@gmail.com>
2021-02-24 19:27:04 -0500

    2nd commit

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Deerfield Academy App Development Club")
             .padding()
+            Image(systemName: "circle")
+        }
     }
 }
 
