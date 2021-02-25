e3d08396d57d3d524a6f25e246a928026f0c0565
andyk21 <andykim2021@gmail.com>
2021-02-24 19:18:19 -0500

    Initial commit

---
+++ TestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProject
+//
+//  Created by Andy Kim on 2/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Andy Kim on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
